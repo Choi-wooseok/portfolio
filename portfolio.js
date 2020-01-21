ab9fc12b7a70467e1dbf49353adb501c8c1ba564
woo_seok <woo_seok10@nate.com>
2020-01-21 18:59:50 +0900

    내용 수정

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,4 @@
-  // main page ======================================
+// main page ======================================
 $(document).on("click", "#menu", () => {
   let menu = document.getElementById("menu");
   if (menu.style.marginLeft === "14%") {
@@ -26,6 +26,58 @@ $(document).on("click", "#contact", () => {
 });
 
 // site page =======================================
+// Awethumb
 $(document).on("click", "#awethumb", () => {
   window.open("http://awethumb.kr");
 });
+$(document).on("mouseover", ".siteLink", () => {
+    $(".siteHover").css("display", "block");
+})
+$(document).on("mouseout", ".siteLink", () => {
+  $(".siteHover").css("display", "none");
+});
+$(document).on("click", "#AwethumbPart", () => {
+  $("#awethumbExplain").css("display", "block");
+  $("#mainResult").css("display", "none");
+  $("#searchResult").css("display", "none");
+  $("#hashtagResult").css("display", "none");
+  $("#commentResult").css("display", "none");
+});
+$(document).on("click", "#MainPage", () => {
+  $("#awethumbExplain").css("display", "none");
+  $("#mainResult").css("display", "block");
+  $("#searchResult").css("display", "none");
+  $("#hashtagResult").css("display", "none");
+  $("#commentResult").css("display", "none");
+});
+$(document).on("click", "#Search", () => {
+  $("#awethumbExplain").css("display", "none");
+  $("#mainResult").css("display", "none");
+  $("#searchResult").css("display", "block");
+  $("#hashtagResult").css("display", "none");
+  $("#commentResult").css("display", "none");
+});
+$(document).on("click", "#Hashtag", () => {
+  $("#awethumbExplain").css("display", "none");
+  $("#mainResult").css("display", "none");
+  $("#searchResult").css("display", "none");
+  $("#hashtagResult").css("display", "block");
+  $("#commentResult").css("display", "none");
+});
+$(document).on("click", "#Comment", () => {
+  $("#awethumbExplain").css("display", "none");
+  $("#mainResult").css("display", "none");
+  $("#searchResult").css("display", "none");
+  $("#hashtagResult").css("display", "none");
+  $("#commentResult").css("display", "block");
+});
+// bitcomu
+$(document).on("click", "#BitPart", () => {
+  $("#BitExplain").css("display", "block");
+  $("#teamResult").css("display", "none");
+});
+$(document).on("click", "#TeamProject", () => {
+  $("#BitExplain").css("display", "none");
+  $("#teamResult").css("display", "block");
+});
+
