d8b77cdee75687113600dc4b6a48e96b7591308f
wooseok <dntjr5843@gmail.com>
2021-01-21 15:25:49 +0900

    no message

--- portfolio.js
+++ portfolio.js
@@ -6,7 +6,7 @@ $(document).on("click", ".bgwrap, #content", (e) => {
   $("#morePage").toggle("hide");
 });
 $(document).on("click", "#goMain", () => {
-  location.href = "/";
+  location.href = "index.html";
 });
 $(document).on("click", "#profile", () => {
   location.href = "profile.html";
